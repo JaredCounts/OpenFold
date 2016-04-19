bba071c242bec8711c587702f069bffccda9b958
Jared Counts <jcounts@mit.edu>
2016-04-19 18:56:08 -0400

    added a util module

---
+++ util.py
@@ -0,0 +1,8 @@
+
+# http://stackoverflow.com/questions/1984799/cross-product-of-two-vectors-in-python
+def cross(a, b):
+    c = [a[1]*b[2] - a[2]*b[1],
+         a[2]*b[0] - a[0]*b[2],
+         a[0]*b[1] - a[1]*b[0]]
+    return c
+
