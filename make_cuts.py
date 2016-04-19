026ee9c0f0faf232a6349a4924216b37c3a78ee4
Jared Counts <jcounts@mit.edu>
2016-04-19 18:11:49 -0400

    started some python files

---
+++ make_cuts.py
@@ -0,0 +1,14 @@
+from notcher import *
+from layout import *
+from slicer import *
+from flexurizer import *
+def make_cuts():
+	# slices = slice(file)
+	# notches = notch(slices)
+	# flexures = flexurize(slices)
+	# for slice in slices:
+	#	slice_notches = notches[slice]
+	# 	slice_flexures = flexures[slice]
+	# 	slice.segments.append(slice_notches)
+	# 	slice.segments.append(slice_flexures)
+	# return layout(slices)
---
+++ notcher.py
@@ -0,0 +1,2 @@
+def notch():
+	print('test')
