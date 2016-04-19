5f536c7453f9a9d94270de92bc1b3d0be44eea88
Jared Counts <jcounts@mit.edu>
2016-04-19 19:51:35 -0400

    determine intersections on sliceA

--- notcher.py
+++ notcher.py
@@ -13,7 +13,31 @@ def notch(slices):
 
 			# vertical axis is the axis that the slices are not perpendicular to
 			verticalAxis = cross(sliceA.axis, sliceB.axis)
-			print(verticalAxis)
+			verticalIndex = 0
+			if verticalAxis[0] != 0:
+				verticalIndex = 0
+			elif verticalAxis[1] != 0:
+				verticalIndex = 1
+			else:
+				verticalIndex = 2
+
+			sliceAIndex = sliceA.axisIndex()
+			sliceBIndex = sliceB.axisIndex()
+
+			# sliceA frame of refence
+			# where (a,b) is (x,y,z) minus sliceA.axis digit
+			# so if sliceA.axis == (0,0,1), then (x,y,z) becomes (x,y)
+			
+			sliceANotchAxisIndex = 0
+			if sliceBIndex < verticalIndex:
+				sliceANotchAxisIndex = 0
+			else:
+				sliceANotchAxisIndex = 1
+			sliceAVerticalAxisIndex = 1 - sliceANotchAxisIndex
+
+			print("on slice A, the notch is where the axis", sliceANotchAxisIndex, "is equal to", sliceB.axis_position)
+
+			
 
 
 
