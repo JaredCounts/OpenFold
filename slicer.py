c5141a879619bb72056ee5b6b65b36184e505bcb
Jared Counts <jcounts@mit.edu>
2016-04-19 19:25:37 -0400

    added function to Slice that can retrieve the index of the axis it is perpendicular to

--- slicer.py
+++ slicer.py
@@ -4,5 +4,17 @@ class Slice(object):
 		self.axis_position = 0
 		self.segments = []
 
+	def axisIndex(self):
+		index = 0
+		if self.axis[0] != 0:
+			index = 0
+		elif self.axis[1] != 0:
+			index = 1
+		else:
+			assert self.axis[2] != 0
+			index = 2
+		return index
+
+
 def slice(file):
 	pass
