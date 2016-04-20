f2a7c1cb7857c6c4d9ff41c591efb272361bc317
Jared Counts <jcounts@mit.edu>
2016-04-19 21:24:59 -0400

    made test_slices a reasonable size

--- test_slices.py
+++ test_slices.py
@@ -4,32 +4,32 @@ from slicer import *
 def test_box():
 	slice1 = Slice()
 	slice1.axis = (1,0,0)
-	slice1.axis_position = 0.25
-	slice1.segments = [( (0,0), (1,0) ),
-					   ( (1,0), (1,1) ),
-					   ( (1,1), (0,1) ),
-					   ( (0,1), (0,0) )]
+	slice1.axis_position = 25
+	slice1.segments = [( (0,0), (100,0) ),
+					   ( (100,0), (100,100) ),
+					   ( (100,100), (0,100) ),
+					   ( (0,100), (0,0) )]
 	slice2 = Slice()
 	slice2.axis = (1,0,0)
-	slice2.axis_position = 0.75
-	slice2.segments = [( (0,0), (1,0) ),
-					   ( (1,0), (1,1) ),
-					   ( (1,1), (0,1) ),
-					   ( (0,1), (0,0) )]
+	slice2.axis_position = 75
+	slice2.segments = [( (0,0), (100,0) ),
+					   ( (100,0), (100,100) ),
+					   ( (100,100), (0,100) ),
+					   ( (0,100), (0,0) )]
 
 	slice3 = Slice()
 	slice3.axis = (0,0,1)
-	slice3.axis_position = 0.25
-	slice3.segments = [( (0,0), (1,0) ),
-					   ( (1,0), (1,1) ),
-					   ( (1,1), (0,1) ),
-					   ( (0,1), (0,0) )]
+	slice3.axis_position = 25
+	slice3.segments = [( (0,0), (100,0) ),
+					   ( (100,0), (100,100) ),
+					   ( (100,100), (0,100) ),
+					   ( (0,100), (0,0) )]
 	slice4 = Slice()
 	slice4.axis = (0,0,1)
-	slice4.axis_position = 0.75
-	slice4.segments = [( (0,0), (1,0) ),
-					   ( (1,0), (1,1) ),
-					   ( (1,1), (0,1) ),
-					   ( (0,1), (0,0) )]
+	slice4.axis_position = 75
+	slice4.segments = [( (0,0), (100,0) ),
+					   ( (100,0), (100,100) ),
+					   ( (100,100), (0,100) ),
+					   ( (0,100), (0,0) )]
 
 	return [slice1, slice2, slice3, slice4]
