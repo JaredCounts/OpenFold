db9f44c5ef16c521f6d243cc4c1df5948d817a59
Anne <annekim94@gmail.com>
2016-04-20 13:22:17 -0400

    added docstring to slicer and added example stl file

--- slicer.py
+++ slicer.py
@@ -1,4 +1,21 @@
 from Slice import *
 
 def slice(file):
+    """
+    input: file -> string file path to stl
+    output: list of Slice, s.t.
+        Slice.axis = 
+            (1,0,0) if perpendicular to x axis or
+            (0,1,0) if perpendicular to y axis or
+            (0,0,1) if perpendicular to z axis
+        Slice.axis_position = distance along Slice.axis from origin in stl file
+        Slice.segments = list of segments, such that
+            Each segment is a tuple of coordinates such that
+            Each coordinate is a tuple of floats
+            Eg. if axis is ‘x’, then they’re (y,z) coordinates
+            Ie. [((a,b), (c,d)), ...]
+    """
+
+
+
 	pass
