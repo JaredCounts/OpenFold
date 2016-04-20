fb161450d625811932852aadbd7aa2cfe855df7c
Jared Counts <jcounts@mit.edu>
2016-04-19 21:24:44 -0400

    removed ad hoc scalar from make_cut

--- make_cuts.py
+++ make_cuts.py
@@ -4,6 +4,7 @@ from slicer import *
 from flexurizer import *
 from test_slices import *
 from util import *
+
 # https://pypi.python.org/pypi/svgwrite/
 import svgwrite
 
@@ -12,20 +13,20 @@ def make_cuts(stlFile, output):
 	slices = test_box() # placeholder
 	notches = notch(slices)
 	# flexures = flexurize(slices)
-	
+
 	for slice in slices:
 		slice_notches = notches[slice]
 		#slice_flexures = flexures[slice]
 		slice.segments.extend(slice_notches)
 		#slice.segments.extend(slice_flexures)
 
-	segments = layout(slices, 0.25)
+	segments = layout(slices, 10)
 	
 	svg = svgwrite.Drawing(output, profile='tiny')
 	for segment in segments:
 		svg.add(svg.line(
-						mult(segment[0],100), 
-						mult(segment[1],100), 
+						segment[0], 
+						segment[1], 
 						stroke=svgwrite.rgb(0, 0, 0, '%'), 
 						stroke_width=4))
 	svg.save()
