2fd0ca3ed61d38df7b52e508439835a45bc62dea
Jared Counts <jcounts@mit.edu>
2016-04-20 23:42:59 -0400

    test slicing with pug

--- make_cuts.py
+++ make_cuts.py
@@ -9,26 +9,26 @@ from util import *
 import svgwrite
 
 def make_cuts(stlFile, svgOutput):
-	# slices = slice(file)
-	slices = test_box() # placeholder
-	notches = notch(slices)
-	# flexures = flexurize(slices)
+	slices = slice(stlFile, 5/10)
+	# slices = test_box() # placeholder
+	# notches = notch(slices)
+	# # flexures = flexurize(slices)
 
-	for slice in slices:
-		slice_notches = notches[slice]
-		#slice_flexures = flexures[slice]
-		slice.segments.extend(slice_notches)
-		#slice.segments.extend(slice_flexures)
+	# for slice in slices:
+	# 	slice_notches = notches[slice]
+	# 	#slice_flexures = flexures[slice]
+	# 	slice.segments.extend(slice_notches)
+	# 	#slice.segments.extend(slice_flexures)
 
-	segments = layout(slices, 10)
+	segments = layout(slices, 5, 400)
 	
 	svg = svgwrite.Drawing(svgOutput, profile='tiny')
 	for segment in segments:
 		svg.add(svg.line(
-						segment[0], 
-						segment[1], 
+						mult(segment[0], 2), 
+						mult(segment[1], 2), 
 						stroke=svgwrite.rgb(0, 0, 0, '%'), 
-						stroke_width=4))
+						stroke_width=2))
 	svg.save()
 
-make_cuts('', 'test.svg')
+make_cuts('pug.stl', 'test.svg')
