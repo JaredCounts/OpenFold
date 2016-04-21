f5149ef2d17b06ac97f659be95c2367dbc7703c1
Jared Counts <jcounts@mit.edu>
2016-04-21 00:31:00 -0400

    added print statements to make_cuts

--- make_cuts.py
+++ make_cuts.py
@@ -9,29 +9,32 @@ from util import *
 import svgwrite
 
 def make_cuts(stlFile, svgOutput):
-	sliceDensity = 2/10
+	sliceDensity = 1/10
+	print("SLICING")
 	slices = slice(stlFile, sliceDensity)
 
+	print("GENERATING NOTCHES")
 	notches = notch(slices)
-	# # flexures = flexurize(slices)
+	# flexures = flexurize(slices)
 
 	for currentSlice in slices:
 	 	slice_notches = notches[currentSlice]
-	# 	#slice_flexures = flexures[slice]
+	 	# slice_flexures = flexures[slice]
 	 	currentSlice.segments.extend(slice_notches)
-	# 	#slice.segments.extend(slice_flexures)
+	 	# slice.segments.extend(slice_flexures)
 
+	print("GENERATING LAYOUT")
 	segments = layout(slices, 10, 400)
 	
 	cutScalingFactor = 2
 
+	print("GENERATING SVG")
 	svg = svgwrite.Drawing(svgOutput, profile='tiny')
 	for segment in segments:
-		svg.add(svg.line(
-						mult(segment[0], cutScalingFactor), 
-						mult(segment[1], cutScalingFactor), 
-						stroke=svgwrite.rgb(0, 0, 0, '%'), 
-						stroke_width=2))
+		svg.add(svg.line(	mult(segment[0], cutScalingFactor), 
+							mult(segment[1], cutScalingFactor), 
+							stroke=svgwrite.rgb(0, 0, 0, '%'), 
+							stroke_width=2))
 	svg.save()
 
-make_cuts('pug.stl', 'test.svg')
+make_cuts('heart.stl', 'heart.svg')
