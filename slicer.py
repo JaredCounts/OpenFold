354356d2cd5a69599316a3bd86f7fd9b456dcbeb
Jared Counts <jcounts@mit.edu>
2016-04-21 00:02:40 -0400

    Can now slice on two different planes

--- slicer.py
+++ slicer.py
@@ -11,63 +11,81 @@ def slice(stlFile, sliceDensity):
 	
 	meshRange = meshMax - meshMin
 	
-	# do x
+	# do x and y
 	sliceXCount = meshRange[0] * sliceDensity
+	sliceYCount = meshRange[1] * sliceDensity
 
 	slices = []
 
-	# we choose two directions to slice from
-	sliceDirection = (1,0,0)
-	sliceIndex = 0
-
 	for xPlane in range(0, int(sliceXCount)):
-		print('slicing', xPlane, 'out of', int(sliceXCount))
+		print('on x: slicing', xPlane, 'out of', int(sliceXCount))
 		x = xPlane * meshRange[0] / int(sliceXCount)
-		
-		sliceSegments = []
+		thisSlice = slice_on_plane(stlMesh, 0, x)
+		if thisSlice is not None:
+			slices.append(thisSlice)
 
-		for triangle in stlMesh:
+	for yPlane in range(0, int(sliceYCount)):
+		print('on y: slicing', yPlane, 'out of', int(sliceYCount))
+		y = yPlane * meshRange[1] / int(sliceYCount)
 
-			v0 = triangle[0:3]
-			v1 = triangle[3:6]
-			v2 = triangle[6:9]
-
-			# skip triangles that don't intersect the plane
-			if v0[0] < x and v1[0] < x and v2[0] < x:
-				continue
-			if v0[0] > x and v1[0] > x and v2[0] > x:
-				continue
-			if v0[0] == x and v1[0] == x and v2[0] == x:
-				continue
-
-			planePoint = [x,0,0]
-			planeNormal = [1,0,0]
-
-			segment1Intersection = intersect_segment_and_plane(v0.tolist(), v1.tolist(), planePoint, planeNormal)
-			segment2Intersection = intersect_segment_and_plane(v0.tolist(), v2.tolist(), planePoint, planeNormal)
-			segment3Intersection = intersect_segment_and_plane(v1.tolist(), v2.tolist(), planePoint, planeNormal)
-
-			segment = []
-			if segment1Intersection != None:
-				segment.append(segment1Intersection[1:3])
-			if segment2Intersection != None:
-				segment.append(segment2Intersection[1:3])
-			if segment3Intersection != None:
-				segment.append(segment3Intersection[1:3])
-
-			if len(segment) != 2:
-				# print("segment not 2 points.", len(segment), "points")
-				continue # something weird happened
-
-			sliceSegments.append(segment)
-
-		thisSlice = Slice()
-		thisSlice.axis = (1,0,0)
-		thisSlice.axis_position = x
-		thisSlice.segments = sliceSegments
-		slices.append(thisSlice)
-
+		thisSlice = slice_on_plane(stlMesh, 1, y)
+		if thisSlice is not None:
+			slices.append(thisSlice)
+			
 	return slices
+
+
+def slice_on_plane(stlMesh, planeIndex, planePosition):
+	sliceSegments = []
+
+	for triangle in stlMesh:
+
+		v0 = triangle[0:3]
+		v1 = triangle[3:6]
+		v2 = triangle[6:9]
+
+		# skip triangles that don't intersect the plane
+		if v0[planeIndex] < planePosition and v1[planeIndex] < planePosition and v2[planeIndex] < planePosition:
+			continue
+		if v0[planeIndex] > planePosition and v1[planeIndex] > planePosition and v2[planeIndex] > planePosition:
+			continue
+		if v0[planeIndex] == planePosition and v1[planeIndex] == planePosition and v2[planeIndex] == planePosition:
+			continue
+
+		planePoint = [0,0,0]
+		planePoint[planeIndex] = planePosition
+
+		planeNormal = [0,0,0]
+		planeNormal[planeIndex] = planePosition
+
+		segment1Intersection = intersect_segment_and_plane(v0.tolist(), v1.tolist(), planePoint, planeNormal)
+		segment2Intersection = intersect_segment_and_plane(v0.tolist(), v2.tolist(), planePoint, planeNormal)
+		segment3Intersection = intersect_segment_and_plane(v1.tolist(), v2.tolist(), planePoint, planeNormal)
+
+		segment = []
+		if segment1Intersection != None:
+			# we extract the relevant 2D coefficients
+			segment.append(segment1Intersection[:planeIndex] + segment1Intersection[planeIndex+1:])
+		if segment2Intersection != None:
+			segment.append(segment2Intersection[:planeIndex] + segment2Intersection[planeIndex+1:])
+		if segment3Intersection != None:
+			segment.append(segment3Intersection[:planeIndex] + segment3Intersection[planeIndex+1:])
+
+		if len(segment) != 2:
+			# print("segment not 2 points.", len(segment), "points")
+			continue # something weird happened
+		sliceSegments.append(segment)
+
+	if len(sliceSegments) == 0:
+		return None
+
+	thisSlice = Slice()
+	thisSlice.axis = [0,0,0]
+	thisSlice.axis[planeIndex] = 1
+	thisSlice.axis_position = planePosition
+	thisSlice.segments = sliceSegments
+
+	return thisSlice
 
 # see if point is between the segment points
 def colinear_point_on_segment(point, segmentPointA, segmentPointB):
