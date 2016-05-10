86b55a83b59ea411639f79027f36078d6e431d0e
Jared Counts <jcounts@mit.edu>
2016-05-09 20:10:51 -0400

    resize model to fit screen on visualizer

--- make_cuts.py
+++ make_cuts.py
@@ -14,6 +14,8 @@ import pygame
 def visualizer(slices, notches, flexures):
 	average = [0,0,0]
 	pointCount = 0
+	minimum = [9999,9999,9999]
+	maximum = [-9999,-9999,-9999]
 	for slice in slices:
 		for segment in slice.segments:
 			startPoint = segment[0].copy()
@@ -22,9 +24,17 @@ def visualizer(slices, notches, flexures):
 			endPoint.insert(slice.axisIndex(), slice.axis_position)
 			average = add(average, startPoint)
 			average = add(average, endPoint)
+			for i in range(0, 3):
+				minimum[i] = min(minimum[i], startPoint[i])
+				maximum[i] = max(maximum[i], startPoint[i])
+				minimum[i] = min(minimum[i], endPoint[i])
+				maximum[i] = max(maximum[i], endPoint[i])
 			pointCount += 2
 
+	mesh_range = diff(maximum, minimum)
+	max_range = max(mesh_range)
 	average = mult(average, 1.0 / pointCount)
+
 
 	# initialize game engine
 	pygame.init()
@@ -36,6 +46,8 @@ def visualizer(slices, notches, flexures):
 	clock = pygame.time.Clock()
 
 	rotateX = 0
+
+	resize = min(size) / max_range * 0.75
 
 	# Loop until the user clicks close button
 	done = False
@@ -57,8 +69,8 @@ def visualizer(slices, notches, flexures):
 				endPoint = segment[1].copy()
 				startPoint.insert(slice.axisIndex(), slice.axis_position)
 				endPoint.insert(slice.axisIndex(), slice.axis_position)
-				pygame.draw.aaline(screen, (0,0,0), toTwoD(diff(startPoint, average), rotateX, size[0]/2, size[1]/2), 
-													toTwoD(diff(endPoint, average), rotateX, size[0]/2, size[1]/2))
+				pygame.draw.aaline(screen, (0,0,0), toTwoD(mult(diff(startPoint, average), resize), rotateX, size[0]/2, size[1]/2), 
+													toTwoD(mult(diff(endPoint, average), resize), rotateX, size[0]/2, size[1]/2))
 
 		# display what’s drawn. this might change.
 		pygame.display.update()
